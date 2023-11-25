03a8f610ff06fe8febb51c9ee0ed9ba10d4445e5
Sameh Hammad <eng.sameh1995@gmail.com>
2023-11-25 14:29:54 -0800

    updating

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,9 @@
-import { cards } from "../lib/data";
-import Card from "../ui/dashboard/card/card";
-import Chart from "../ui/dashboard/chart/chart";
-import styles from "../ui/dashboard/dashboard.module.css";
-import Rightbar from "../ui/dashboard/rightbar/rightbar";
-import Transactions from "../ui/dashboard/transactions/transactions";
+import { cards } from "@/app/lib/data";
+import Card from "@/app/ui/dashboard/card/card";
+import Chart from "@/app/ui/dashboard/chart/chart";
+import styles from "@/app/ui/dashboard/dashboard.module.css";
+import Rightbar from "@/app/ui/dashboard/rightbar/rightbar";
+import Transactions from "@/app/ui/dashboard/transactions/transactions";
 
 const Dashboard = () => {
   return (
