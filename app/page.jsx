399f77afc967da1e19b199946d8e1a8bff561022
Sameh Hammad <eng.sameh1995@gmail.com>
2024-01-28 16:02:56 -0800

    update paggination

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,10 @@
-"use client";
-import { useMode } from "./ModeContext/ModeContext";
+
 import "@/app/ui/globals.css";
 const Homepage = () => {
-  const { mode, toggleMode } = useMode();
 
   return (
     <div className="">
-      <h1
-        onClick={() => toggleMode()}
-        className={`text-3xl font-bold underline text-center ${
-          mode === "dark" ? "dark" : "light"
-        }`}
-      >
-        Hello world!
-      </h1>
+  
     </div>
   );
 };
